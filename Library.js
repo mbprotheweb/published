5de435ccabed50d8ff770b36b31dc3826b42864c
mbPro <100542453+mbprotheweb@users.noreply.github.com>
2023-05-05 18:30:26 -0400

    change to allow cors

--- Library.js
+++ Library.js
@@ -1,6 +1,11 @@
 async function run(perm) {
   const url = 'https://script.google.com/macros/s/AKfycbxvML-G-VG_WB5HLYS8joDK_50-1L1HTxdajcjSNOTDTJYqisnz7yVG9KRE1ggcsIs8/exec?URL=' + encodeURIComponent(perm);
-  fetch(url);
+  fetch(url, {
+    redirect: "follow",
+    headers: {
+      "Content-Type": "text/plain;charset=utf-8",
+    }
+  });
 }
 
 var a = document.getElementById("recordnum")?.href;
