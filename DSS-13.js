c818f9131b853f485836e1aee0db7abb54c7e583
mbPro <100542453+mbprotheweb@users.noreply.github.com>
2024-01-12 01:15:28 -0500

    Update DSS-13.js

--- DSS-13.js
+++ DSS-13.js
@@ -45,9 +45,6 @@ var elements = document.querySelectorAll('textarea[data-field-native-placeholder
     setNativeValue(ele, ans);
   }
 });
-function getElementIndex(element) {
-  return Array.from(element.parentNode.children).indexOf(element);
-}
 
 var ans = [1, 0, 0, 1, 0, 0, 2, 6, 0, 2, 2, 1, 2, 0, 2, 2, 1, 1, 3, 3, 2, 2, 0, 1, 2, 3, 2, 0, 0, 2, 0, 3, 2, 1, 3, 3, 0, 2, 3, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 3, 2, 3, 0, 1, 1, 0, 0, 3, 2, 3, 1, 3, 0, 1, 2, 0, 0, 3, 3, 2, 3, 0, 0, 1, 0, 1];
 var belements = document.querySelectorAll('form[data-radio-buttons-field-options-form="true"]');
